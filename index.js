3434140e72d6408c486d5ebd031804fc570d7293
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-08 10:53:50 +0300

    commit tree dec7ca141dd644bcf25744edd708867be0f454ae

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+//if ..elseif...else statement
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return "This one is on me!";
+  }
+  
+  else if (someValue > 2500){
+    return "No can do.";
+  }
+  else if (someValue > 2000){
+    return "I will gladly take your thirty bucks.";
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+console.log(scuberGreetingForFeet(200));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+//ternary operator
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+console.log(ternaryCheckCity("Kenya"));
+
+
+//switch statement
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."    
+  }
+}
+console.log(switchOnCharmFromTip("generous"));
+
+
+
+
